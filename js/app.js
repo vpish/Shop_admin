c4aa5bdf6746b73b6e0a373217719e25c3383a1d
vpish <vpish02@hotmail.com>
2015-09-28 03:09:50 +0300

    added routing

--- js/app.js
+++ js/app.js
@@ -1,37 +1,34 @@
 (function() {
 	var app = angular.module('ShopAdmin', ['ngRoute']);
 
-	app.controller('AppController', function() {
+	app.config(['$routeProvider','$locationProvider', function ($routeProvider, $locationProvider) {
+		$locationProvider.html5Mode({enabled: true, requireBase: false});
+		$routeProvider
+			.when('/', {templateUrl: '/main.html'})
+			.when('/customers', {templateUrl: '/customers.html'})
+			.when('/products', {templateUrl: '/products.html'})
+			.when('/shoppingcart', {templateUrl: '/shoppingcart.html'})
+			.when('/contacts', {templateUrl: '/contacts.html'});
+	}]);
 
-	});
+	app.controller('AppController',['$location', function($location){
+		
+		this.currentMenu='main';
+		this.changePath = function(path){
+			$location.path(path);
+			if (path === '/') {this.currentMenu='main';}
+			else if (path === '/customers') {this.currentMenu='customers';}
+			else if (path === '/products') {this.currentMenu='products';}
+			else if (path === '/contacts') {this.currentMenu='contacts';}
+			else if (path === '/shoppingcart') {this.currentMenu='shoppingcart';}
+			
+		}; 
+		
+			
+	}]);
 
-	app.directive('customersPage', function() {
-		return {
-			restrict : 'E',
-			templateUrl : 'customers.html'
-		};
-	});
 
-	app.directive('productsPage', function() {
-		return {
-			restrict : 'E',
-			templateUrl : 'products.html'
-		};
-	});
 
-	app.directive('shoppingcartPage', function() {
-		return {
-			restrict : 'E',
-			templateUrl : 'shoppingcart.html'
-		};
-	});
-
-	app.directive('contactsPage', function() {
-		return {
-			restrict : 'E',
-			templateUrl : 'contacts.html'
-		};
-	});
 	
 	
 })();
